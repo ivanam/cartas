a81dfe456a7ffdf3755edadc12492d35076bcb1e
Leandro <lealuque.tw@gmail.com>
2016-05-20 20:16:23 -0300

    Mia

--- public/static/js/procesamiento_carta.js
+++ public/static/js/procesamiento_carta.js
@@ -1,36 +1,17 @@
 $("#btnGuardar").click(function(){
 
 	if ($("#nombre_archivo").val() == ""){
-		alert("Debe ingresar un nombre de archivo");
+		var modal = $("#modal");
+		modal.find(".modal-title").text("Error");
+		modal.find(".modal-body p").text("Debe colocarle un nombre a la carta");
+		modal.find(".modal-header").attr({"style":"background-color:#c77270"});
+		modal.find("h4").attr({"style":"color: black"});
+		modal.modal("show");
 	}else{
 		crearCarta($("#carta").html(),$("#nombre_archivo").val());
 	}
 	
 });
-
-
-// $("#btnEnviar").click(function(){
-// 	var modal = $("#mails");
-// 	modal.modal("show");
-// });
-
-// $("#btnEnviarModal").click(function(){
-
-// 	var contenido = { contenido:$("#carta").html(),
-// 					  destino:$("#destinatario").val(),
-// 					};
-// 	$.ajax({
-// 		type:"post",
-// 		url:"/enviar",
-// 		headers: {
-//     		'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-//   		},
-// 		data:contenido,
-// 		complete:function(data){
-// 			$("#mails").modal("hide");
-// 		}
-// 	});
-// });
 
 function crearCarta(contenido, nombre_archivo){
 	var contenido = {"contenido":contenido,
